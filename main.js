2398918f3cf8ac420dda0df26da378aa9fb6f889
Kirby Mekler <kirbymekler@Kirbys-MacBook-Air.local>
2014-10-10 11:28:30 -0700

    committing with angular functionality

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-var app = angular.module('ticTacToe', [];)
+var TTTApp = angular.module('TTTApp', []);
 
-app.controller=('ticTacController', function ($scope){
+TTTApp.controller('TTTController', function ($scope) {
 
-}
-});
+  $scope.cellList = [
+
+  {status: "A", image: [{name: 'marcia', src: 'marcia_new.png'}], num: 1}, 
+
+  {status: "B", image: [{name: 'carol', src: 'carol_new.png'}], num: 2}, 
+  {status: "C", image: [{name: 'greg', src: 'greg_new.png'}], num: 3}, 
+  {status: "D", image: [{name: 'jan', src: 'jan_new.png'}], num: 4}, 
+  {status: "E", image: [{name: 'alice', src: 'alice_new.png'}], num: 5}, 
+  {status: "F", image: [{name: 'peter', src: 'peter_new.png'}], num: 6}, 
+  {status: "G", image: [{name: 'cindy', src: 'cindy_new.png'}], num: 7}, 
+  {status: "H", image: [{name: 'mike', src: 'mike_new.png'}], num: 8}, 
+  {status: "I", image: [{name: 'bobby', src: 'bobby_new.png'}], num: 9}
+  ]  ;
+
+  $scope.movecounter = 0 ;
+
+  $scope.playerPicks = function(thisCell) {
+    $scope.movecounter = $scope.movecounter + 1 ;
+    console.log("Cell was: " + thisCell.status) ;
+    if (($scope.movecounter % 2) == 1) {
+      thisCell.status = "X" ;  
+    } else {
+      thisCell.status = "O" ;
+    } 
+    console.log("Cell is now: " + thisCell.status) ;
+  } ;
+
+
+}) ;
